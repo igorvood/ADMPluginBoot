bdaa9f160070aff6a3ef5092910d3d420244321f
balitsky_igor <balitsky_igor@list.ru>
2018-07-15 20:24:06 +0300

    рефакторинг

--- src/main/kotlin/ru/vood/admplugin/infrastructure/applicationConst/Const.java
+++ src/main/kotlin/ru/vood/admplugin/infrastructure/applicationConst/Const.java
@@ -4,4 +4,7 @@ public class Const {
     public final static String COLLECTION = "COLLECTIONID";
     public final static String INDEX_PREFIX = "IDX_Z#";
 
+    public static final String CONNECTIONS = "Connections.gson";
+    public static final String DB_PROPERTUES = "DBProperty.properties";
+
 }
